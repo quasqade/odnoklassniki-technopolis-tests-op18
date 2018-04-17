b1fe4fe9734ecc5aa7b0c5034b3b454d22e75eed
quasqade <quasqade@gmail.com>
2018-04-17 22:22:10 +0300

    добавил новые методы в base

--- odnoklassniki-technopolis-tests-op18/src/test/java/core/pages/PageBase.java
+++ odnoklassniki-technopolis-tests-op18/src/test/java/core/pages/PageBase.java
@@ -1,10 +1,15 @@
 package core.pages;
 
+import com.google.common.base.Preconditions;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -62,4 +67,65 @@ public abstract class PageBase {
       acceptNextAlert = true;
     }
   }
+
+
+  /**
+   * Ожидание
+   */
+  public boolean explicitWait(final ExpectedCondition<?> condition, long maxCheckTimeInSeconds,
+      long millisecondsBetweenChecks) {
+    Preconditions.checkNotNull(condition, "Condition must be not null");
+    Preconditions.checkArgument(TimeUnit.MINUTES.toSeconds(3) > maxCheckTimeInSeconds,
+        "Max check time in seconds should be less than 3 minutes");
+    checkConditionTimeouts(maxCheckTimeInSeconds, millisecondsBetweenChecks);
+    try {
+      // сбрасываем ожидания в 0
+      driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      // создаем эксплицитное ожидание
+      WebDriverWait explicitWait = new WebDriverWait(driver, maxCheckTimeInSeconds,
+          millisecondsBetweenChecks);
+      // проверяем его
+      explicitWait.until(condition);
+      return true;
+    } catch (Exception e) {
+      return false;
+    } finally {
+      // при любом результате восстанавливаем значение имплицитного ожидания по умолчанию
+      if (driver != null) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      } else {
+        throw new IllegalArgumentException("Driver shouldnt be null");
+      }
+    }
+  }
+
+  /**
+   * Проверяет таймаут провекри условия и интервал между проверками: таймаут должен быть больше
+   * нуля, интервал проверки должен быть больше нуля интервал между проверками умноженный на 1000
+   * должен быть меньше таймаута проверки
+   *
+   * @param maxCheckTimeInSeconds максимальное время проверки в секундах
+   * @param millisecondsBetweenChecks интервал между проверками в милисекундах
+   */
+  private void checkConditionTimeouts(long maxCheckTimeInSeconds, long millisecondsBetweenChecks) {
+    Preconditions
+        .checkState(maxCheckTimeInSeconds > 0, "maximum check time in seconds must be not 0");
+    Preconditions.checkState(millisecondsBetweenChecks > 0,
+        "milliseconds count between checks must be not 0");
+    Preconditions.checkState(millisecondsBetweenChecks < (maxCheckTimeInSeconds * 1000),
+        "Millis between checks must be less than max seconds to wait");
+  }
+
+  public void moveToElement(WebElement webElement) {
+    new Actions(driver).moveToElement(webElement).build().perform();
+  }
+
+  protected boolean isElementVisible(By by) {
+    try {
+      return driver.findElement(by).isDisplayed();
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
 }
+
